73e04a36d598fc2da8f09311fbcf7352d5e91d66
zhengxiawu <zhengxiawu@126.com>
2020-06-05 16:26:48 +0800

    bug fixing

--- pytorch_cifar_v2/train.py
+++ pytorch_cifar_v2/train.py
@@ -17,7 +17,7 @@ device = torch.device("cuda")
 writer = SummaryWriter(log_dir=os.path.join(config.path, "tb"))
 writer.add_text('config', config.as_markdown(), 0)
 
-logger = utils.get_logger(os.path.join(config.path, "{}.log".format(config.name)))
+logger = utils.get_logger(os.path.join(config.path, "logger.log"))
 config.print_params(logger.info)
 
 
