840aed3b70716b535333c280ab413785e8d579d4
zhengxiawu <zhengxiawu@126.com>
2020-06-05 20:35:59 +0800

    add network

--- pytorch_cifar_v2/models/__init__.py
+++ pytorch_cifar_v2/models/__init__.py
@@ -1,9 +1,17 @@
 from .nas_models import DARTS_V1, DARTS_V2, AmoebaNet, NASNet
+from .resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152, Basic_res_cell, BottleNeck_res_cell
 model_dict = {
     'darts_v1': DARTS_V1(),
     'darts_v2': DARTS_V2(),
     'amoebaNet': AmoebaNet(),
-    'nasnet': NASNet()
+    'nasnet': NASNet(),
+    'resnet18': ResNet18(),
+    'resnet34': ResNet34(),
+    'resnet50': ResNet50(),
+    'resnet101': ResNet101(),
+    'resnet152': ResNet152(),
+    'basic_resnet_cell': Basic_res_cell(),
+    'bottleneck_resnet_cell': BottleNeck_res_cell()
 }
 
 
