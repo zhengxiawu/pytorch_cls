1074bb18ebd5a1573b0ede934f0fdfb53e9cd736
zhengxiawu <zhengxiawu@126.com>
2020-06-06 09:49:23 +0800

    add log line

--- pytorch_cifar_v2/models/resnet.py
+++ pytorch_cifar_v2/models/resnet.py
@@ -54,8 +54,7 @@ class Bottleneck(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion *
-                               planes, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
