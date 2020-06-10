026426c45eabbfca09ceea2d2cf41a46f4782330
zhengxiawu <zhengxiawu@126.com>
2020-06-10 17:32:46 +0800

    add network

--- pytorch_cifar_v2/models/__init__.py
+++ pytorch_cifar_v2/models/__init__.py
@@ -1,5 +1,12 @@
 from .nas_models import DARTS_V1, DARTS_V2, AmoebaNet, NASNet
-from .resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152, Basic_res_cell, BottleNeck_res_cell, BottleNeck_rescell_36_3_20, Basic_rescell_36_3_20
+from .preact_resnet import (PreBasic_res_cell_64_2_20,
+                            PreBasic_rescell_36_3_20,
+                            PreBottleNeck_res_cell_64_2_20,
+                            PreBottleNeck_rescell_36_3_20)
+from .resnet import (Basic_res_cell, Basic_rescell_36_3_20,
+                     BottleNeck_res_cell, BottleNeck_rescell_36_3_20, ResNet18,
+                     ResNet34, ResNet50, ResNet101, ResNet152)
+
 model_dict = {
     'darts_v1': DARTS_V1(),
     'darts_v2': DARTS_V2(),
@@ -13,7 +20,11 @@ model_dict = {
     'basic_resnet_cell': Basic_res_cell(),
     'bottleneck_resnet_cell': BottleNeck_res_cell(),
     'basic_rescell_36_3_20': Basic_rescell_36_3_20(),
-    'bottleNeck_rescell_36_3_20': BottleNeck_rescell_36_3_20()}
+    'bottleNeck_rescell_36_3_20': BottleNeck_rescell_36_3_20(),
+    'PreBasic_res_cell_64_2_20': PreBasic_res_cell_64_2_20(),
+    'PreBottleNeck_res_cell_64_2_20': PreBottleNeck_res_cell_64_2_20(),
+    'PreBasic_rescell_36_3_20': PreBasic_rescell_36_3_20(),
+    'PreBottleNeck_rescell_36_3_20': PreBottleNeck_rescell_36_3_20()}
 
 
 def get_model(model_name):
