78151cd3904a7c27fdd377322078bd131c3ac3f7
zhengxiawu <zhengxiawu@126.com>
2020-06-10 11:10:04 +0800

    test

--- pytorch_cifar_v2/log_reader.py
+++ pytorch_cifar_v2/log_reader.py
@@ -9,9 +9,15 @@ if __name__ == "__main__":
     experiment_path = os.path.abspath('./experiments')
     model_list = os.listdir(experiment_path)
     model_list.sort()
+    md_str = '|'
     for model in model_list:
         if not model[0] == '.':
-            print(model)
+            _list = model.split('_')
+            if _list[0] == 'darts':
+                md_str += _list[0] + '_' + _list[1]
+            else:
+                md_str += _list[0]
+            md_str += "|"
             model_log_path = os.path.join(experiment_path, model, 'logger.log')
             if os.path.isfile(model_log_path):
                 with open(model_log_path) as f:
@@ -19,15 +25,25 @@ if __name__ == "__main__":
                 start_time = datetime.strptime(log_lines[0][0:17], '%m/%d %I:%M:%S %p')
                 end_time = datetime.strptime(log_lines[-1][0:17], '%m/%d %I:%M:%S %p')
                 training_hours = (end_time - start_time).total_seconds() / 3600.
-                print("Training times is {0}".format(training_hours))
-                print("Hyper-parameters is:")
-                print(log_lines[2])
-                print(log_lines[4])
-                print(log_lines[7])
                 for line in log_lines:
                     if 'Model size' in line:
-                        print(line)
+                        model_size = line.split('=')[-1]
                     if 'FLOPs' in line:
-                        print(line)
-                print(log_lines[-1])
+                        flops = line.split('=')[-1]
+                md_str += log_lines[-1].split('=')[-1]
+                md_str += '|'
+                md_str += model_size
+                md_str += '|'
+                md_str += flops
+                md_str += '|'
+                md_str = md_str + str(training_hours) + '|'
+                md_str = md_str + log_lines[2].split('=')[-1] + '|'
+                md_str = md_str + log_lines[7].split('=')[-1] + '|'
+                md_str = md_str + log_lines[4].split('=')[-1] + '|'
+                # print("Hyper-parameters is:")
+                # print(log_lines[2])
+                # print(log_lines[4])
+                # print(log_lines[7])
+                
+                # print(log_lines[-1])
 
