b5e78f130c0b4b938669a758d093b3403edb7bc3
zhengxiawu <zhengxiawu@126.com>
2020-06-18 23:59:27 +0800

    bug fixing

--- pytorch-imagenet-basic/test/data_loader_test.py
+++ pytorch-imagenet-basic/test/data_loader_test.py
@@ -14,13 +14,12 @@ def time_test(loader):
     overall_batch = []
     pre_tic_time = time.time()
     for cur_iter, (inputs, labels) in enumerate(loader):
-        if cur_iter > 200:
-            break
         tic_time = time.time()
         # Transfer the data to the current GPU device
         inputs, labels = inputs.cuda(), labels.cuda(non_blocking=True)
         batch_time = tic_time - pre_tic_time
         pre_tic_time = tic_time
+        # print(batch_time)
         print(batch_time)
         overall_batch.append(batch_time)
         # print('one batch time is:{}'.format(str(batch_time)))
@@ -29,7 +28,7 @@ def time_test(loader):
 
 
 if __name__ == "__main__":
-    num_workers = 4
+    # num_workers = 4
     # print('data_loader test')
     # print('using default dataloader')
     data_path = '/gdata/ImageNet2012'
@@ -44,54 +43,54 @@ if __name__ == "__main__":
     # mean, std = time_test(loader)
     # print('mean time is:{}'.format(mean))
     # print('time std is:{}'.format(std))
+    for num_workers in range(4, 48, 4):
+        print('using DALI GPU dataloader, Finetune workers')
+        dataset = ImageNet_Dataset(data_path,
+                                   batch_size=256,
+                                   size=224,
+                                   val_batch_size=200,
+                                   val_size=256,
+                                   min_crop_size=0.08,
+                                   workers=num_workers,
+                                   world_size=1,
+                                   cuda=True,
+                                   use_dali=True,
+                                   dali_cpu=False)
+        loader = dataset.train_loader
+        mean, std = time_test(loader)
+        print('mean time is:{}'.format(mean))
+        print('time std is:{}'.format(std))
 
-    print('using DALI GPU dataloader')
-    dataset = ImageNet_Dataset(data_path,
-                               batch_size=256,
-                               size=224,
-                               val_batch_size=200,
-                               val_size=256,
-                               min_crop_size=0.08,
-                               workers=num_workers,
-                               world_size=1,
-                               cuda=True,
-                               use_dali=True,
-                               dali_cpu=False)
-    loader = dataset.train_loader
-    mean, std = time_test(loader)
-    print('mean time is:{}'.format(mean))
-    print('time std is:{}'.format(std))
+    # print('using DALI CPU dataloader')
+    # dataset = ImageNet_Dataset(data_path,
+    #                            batch_size=256,
+    #                            size=224,
+    #                            val_batch_size=200,
+    #                            val_size=256,
+    #                            min_crop_size=0.08,
+    #                            workers=num_workers,
+    #                            world_size=1,
+    #                            cuda=True,
+    #                            use_dali=True,
+    #                            dali_cpu=True)
+    # loader = dataset.train_loader
+    # mean, std = time_test(loader)
+    # print('mean time is:{}'.format(mean))
+    # print('time std is:{}'.format(std))
 
-    print('using DALI CPU dataloader')
-    dataset = ImageNet_Dataset(data_path,
-                               batch_size=256,
-                               size=224,
-                               val_batch_size=200,
-                               val_size=256,
-                               min_crop_size=0.08,
-                               workers=num_workers,
-                               world_size=1,
-                               cuda=True,
-                               use_dali=True,
-                               dali_cpu=True)
-    loader = dataset.train_loader
-    mean, std = time_test(loader)
-    print('mean time is:{}'.format(mean))
-    print('time std is:{}'.format(std))
-
-    print('using Torch CPU dataloader')
-    dataset = ImageNet_Dataset(data_path,
-                               batch_size=256,
-                               size=224,
-                               val_batch_size=200,
-                               val_size=256,
-                               min_crop_size=0.08,
-                               workers=num_workers,
-                               world_size=1,
-                               cuda=True,
-                               use_dali=False,
-                               dali_cpu=True)
-    loader = dataset.train_loader
-    mean, std = time_test(loader)
-    print('mean time is:{}'.format(mean))
-    print('time std is:{}'.format(std))
+    # print('using Torch CPU dataloader')
+    # dataset = ImageNet_Dataset(data_path,
+    #                            batch_size=256,
+    #                            size=224,
+    #                            val_batch_size=200,
+    #                            val_size=256,
+    #                            min_crop_size=0.08,
+    #                            workers=num_workers,
+    #                            world_size=1,
+    #                            cuda=True,
+    #                            use_dali=False,
+    #                            dali_cpu=True)
+    # loader = dataset.train_loader
+    # mean, std = time_test(loader)
+    # print('mean time is:{}'.format(mean))
+    # print('time std is:{}'.format(std))
