8d075f63b84c68fbab683a79aa98b637c44bc243
zhengxiawu <zhengxiawu@126.com>
2020-06-23 14:42:05 +0800

    add pca plot

--- pytorch-imagenet-basic/tools/plot_training_result.py
+++ pytorch-imagenet-basic/tools/plot_training_result.py
@@ -2,8 +2,9 @@ from pycls.core.plotting import plot_error_curves_plotly, plot_error_curves_pypl
 import argparse
 
 if __name__ == "__main__":
-    log_file_name = ['./experiments/EN-B0_dds_4gpu/stdout.log']
-    name = ['efficientB0']
-    filename = './experiments/EN-B0_dds_4gpu/result.html'
+    log_file_name = ['./experiments/EN-B0_dds_4gpu/stdout.log',
+                     './experiments/EN-B0_dds_4gpu_color_PCA/stdout.log', ]
+    name = ['efficientB0', 'efficientB0_PCA']
+    filename = './experiments/EN-B0_dds_4gpu/result_.html'
     plot_error_curves_plotly(log_file_name, name, filename)
     pass
