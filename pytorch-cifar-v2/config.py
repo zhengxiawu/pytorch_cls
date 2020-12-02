5c066a0bf18c6ff93df1a4cfe44dd2202a4f3ef7
zhengxiawu <zhengxiawu@126.com>
2020-12-02 20:30:38 +0800

    add res depth

--- pytorch-cifar-v2/config.py
+++ pytorch-cifar-v2/config.py
@@ -75,7 +75,8 @@ class AugmentConfig(BaseConfig):
                             default=0.2, help='drop path prob')
         parser.add_argument('--drop_out', type=float,
                             default=0, help='drop out rate')
-        parser.add_argument('--autoaugment', action='store_true', help='use auto augmentation')
+        parser.add_argument(
+            '--autoaugment', action='store_true', help='use auto augmentation')
         parser.add_argument('--genotype', type=str,
                             default=None, help='genotype of nasnet')
         return parser
@@ -88,5 +89,66 @@ class AugmentConfig(BaseConfig):
         self.data_path = '/gdata/cifar10'
         _path = "{}_{}_{}".format(
             self.model_name, time.strftime("%Y%m%d-%H%M%S"), torch.__version__)
+        self.path = os.path.join('./experiments', _path)
+        self.gpus = parse_gpus(self.gpus)
+
+
+class Depth_AugmentConfig(BaseConfig):
+    def build_parser(self):
+        parser = get_parser("Augment config")
+        parser.add_argument('--model_name', default='depth_resnet')
+        # architecture
+        parser.add_argument('--channel', type=int, default=64, help='channel')
+        parser.add_argument('--layers', type=int,
+                            default=20, help='# of layers')
+        parser.add_argument('--factor', type=int,
+                            default=2, help='# channel multi factor')
+        parser.add_argument('--expansion', type=int,
+                            default=1, help='# channel expansion')
+        parser.add_argument('--dataset', default='CIFAR10',
+                            help='CIFAR10 / MNIST / FashionMNIST')
+        parser.add_argument('--batch_size', type=int,
+                            default=96, help='batch size')
+        parser.add_argument('--lr', type=float,
+                            default=0.025, help='lr for weights')
+        parser.add_argument('--momentum', type=float,
+                            default=0.9, help='momentum')
+        parser.add_argument('--weight_decay', type=float,
+                            default=3e-4, help='weight decay')
+        parser.add_argument('--grad_clip', type=float, default=5.,
+                            help='gradient clipping for weights')
+        parser.add_argument('--print_freq', type=int,
+                            default=200, help='print frequency')
+        parser.add_argument('--gpus', default='0', help='gpu device ids separated by comma. '
+                            '`all` indicates use all gpus.')
+        parser.add_argument('--epochs', type=int, default=600,
+                            help='# of training epochs')
+        parser.add_argument('--init_channels', type=int, default=36)
+
+        parser.add_argument('--seed', type=int, default=2, help='random seed')
+        parser.add_argument('--workers', type=int,
+                            default=4, help='# of workers')
+        parser.add_argument('--aux_weight', type=float,
+                            default=0.4, help='auxiliary loss weight')
+        parser.add_argument('--cutout_length', type=int,
+                            default=16, help='cutout length')
+        parser.add_argument('--drop_path_prob', type=float,
+                            default=0.2, help='drop path prob')
+        parser.add_argument('--drop_out', type=float,
+                            default=0, help='drop out rate')
+        parser.add_argument(
+            '--autoaugment', action='store_true', help='use auto augmentation')
+        parser.add_argument('--genotype', type=str,
+                            default=None, help='genotype of nasnet')
+        return parser
+
+    def __init__(self):
+        parser = self.build_parser()
+        args = parser.parse_args()
+        super().__init__(**vars(args))
+
+        self.data_path = '/gdata/cifar10'
+        _path = "{}_{}_{}_{}_{}_{}_{}".format(
+            self.model_name, self.channel, self.layers, self.factor, self.expansion, time.strftime("%Y%m%d-%H%M%S"), torch.__version__)
         self.path = os.path.join('./experiments', _path)
         self.gpus = parse_gpus(self.gpus)
